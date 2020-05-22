348259f853c73d33620c42e071ec90cc3504a227
Radu Nita <radu.nita@titanium-soft.com>
2020-05-22 14:47:13 +0300

    Support usage as CLI utility

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 const BitcoinNetwork = require('./BitcoinNetwork');
 const EthereumNetwork = require('./EthereumNetwork');
 
-const {
-  BLOCKCYPHER_TOKEN,
-  INFURA_TOKEN,
-  NETWORK_TYPE,
-  SENDER_WIF,
-  RECIPIENT_ADDRESS,
-  AMOUNT,
-} = process.env;
+if (!module.parent) {
+  // eslint-disable-next-line global-require
+  const repl = require('repl');
 
-
-(async () => {
-  const ethereum = new EthereumNetwork({
-    networkType: NETWORK_TYPE,
-    accessToken: INFURA_TOKEN,
+  const r = repl.start('> ');
+  Object.defineProperties(r.context, {
+    BitcoinNetwork: { configurable: false, enumerable: true, value: BitcoinNetwork },
+    EthereumNetwork: { configurable: false, enumerable: true, value: EthereumNetwork },
   });
-
-  const bitcoin = new BitcoinNetwork({
-    networkType: NETWORK_TYPE,
-    accessToken: BLOCKCYPHER_TOKEN,
-  });
-
-  const { privateKey } = ethereum.generateAccount();
-  console.log('Random Ethereum account generated. pkey:', privateKey);
-  console.log('=====');
-
-  await ethereum.connect(privateKey);
-  console.log('Current Ethereum account:', ethereum.defaultAccount);
-  console.log('=====');
-
-  await bitcoin.connect(SENDER_WIF);
-  console.log('Current Bitcoin account:', bitcoin.defaultAccount);
-  console.log('=====');
-
-  const tx = await bitcoin.createTransaction(
-    bitcoin.defaultAccount, RECIPIENT_ADDRESS, AMOUNT,
-  );
-  console.log('Transaction was created. tx:', tx);
-  console.log('=====');
-
-  const rawTx = await bitcoin.signTransaction(tx);
-  console.log('Transaction was signed. Raw tx hex:', rawTx);
-  console.log('=====');
-
-  try {
-    const txHash = await bitcoin.broadcastTransaction(rawTx);
-    console.log('Transaction was broadcasted. txHash:', txHash);
-  } catch (e) {
-    console.log('Unable to broadcasted the transaction. ', e.message);
-  }
-  console.log('=====');
-})();
+} else {
+  module.exports = {
+    BitcoinNetwork,
+    EthereumNetwork,
+  };
+}
